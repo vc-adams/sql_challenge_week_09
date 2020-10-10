22590701f505c726736e5d2b99de79b8245604f3
Change vc-adams <vincent.c.adams@gmail.com>
2020-10-10 11:53:24 -0500

    Edited bonus questions.

--- EmployeeSQL/employees_db_queries_VCA.sql
+++ EmployeeSQL/employees_db_queries_VCA.sql
@@ -93,4 +93,55 @@ SELECT
 	FROM employees
 	COUNT emp_lst_nm;
 
+
+
+
+SELECT ttl AS "Title", COUNT(employees.emp_no) 
+	FROM titles GROUP BY ttl
+		WHERE titles.ttl_id = employees.emp_no;
+
+
+SELECT COUNT(emp_no)
+	From employees 
+		WHERE emp_title_id = 
+		(
+			SELECT ttl AS "Title" 
+				FROM titles GROUP BY ttl
+		)
+
+
+	
+
+-- Bonus
+
+-- 2. Create a histogram to visualize the most common salary ranges for employees.
+
+SELECT slry AS "Salaries", COUNT(emp_no) AS "Employee Count"
+         FROM salaries GROUP BY slry
+		 ORDER BY "Employee Count" DESC
+		 LIMIT 20;
+
+
+-- 3. Create a bar chart of average salary by title.
+
+SELECT titles.ttl, ROUND(AVG(salaries.slry), 2)
+	FROM employees
+		JOIN titles
+			ON titles.ttl_id = employees.emp_title_id	
+		JOIN salaries
+			ON salaries.emp_no = employees.emp_no
+		GROUP BY titles.ttl;
 		
+		
+-- Epilogue
+-- Evidence in hand, you march into your boss's office and present the visualization. 
+-- With a sly grin, your boss thanks you for your work. On your way out of the office, you hear the words, 
+-- "Search your ID number." You look down at your badge to see that your employee ID number is 499942.
+
+SELECT titles.ttl, salaries.slry
+	FROM employees
+		JOIN titles
+			ON titles.ttl_id = employees.emp_title_id	
+		JOIN salaries
+			ON salaries.emp_no = employees.emp_no
+		WHERE employees.emp_no = 499942
